2b3b2e0b42c17f1564e55d77cc6ef3ff9dce353c
Stephanie Santiso <stephlovesdanny@nitrogen-2.local>
2016-05-20 16:33:33 -0400

    Interactivity to Relaxr blog

--- week6/homework/starter_code/js/main.js
+++ week6/homework/starter_code/js/main.js
@@ -2,18 +2,34 @@
 $(document).on('ready', function() {
   $('.readmore').on('click', handleReadMore);
   $('.readless').on('click', handleReadLess);
+  $('.learnmore').on('click', handleLearnMore);
 });
 
 
 function handleReadMore(event) {
   event.preventDefault();
- $('.hide').slideDown().show();
- $('.readmore').hide();
+  $(event.target)
+    .closest('.post')
+    .find('.hide')
+    .slideDown()
+    .show();
+ // $('.hide').slideDown().show();
+ $(event.target).closest('.readmore').slideUp();
 }
 
 function handleReadLess(event) {
   event.preventDefault();
-  $('.hide').slideUp();
-  $('.readmore').show();
+  $(event.target)
+  .closest('.post')
+  .find('.hide')
+  .slideUp();
+  $(event.target).closest('.readmore').slideDown(); 
 
 }
+
+function handleLearnMore(event) {
+  event.preventDefault();
+  $('#sidebar .hide').show();
+  $('.learnmore').hide();
+}
+
