a820faa106f584ef665168c4e892eacaa7b94305
Stephanie Santiso <stephlovesdanny@nitrogen-2.local>
2016-05-21 09:52:41 -0400

    Final Project Outline (partial outline)

---
+++ FinalProject/js/bloglistpage.js
@@ -0,0 +1,7 @@
+/*
+After page loads 3 most recent blog posts will appear in the center.
+  On hover title will appear underlined.
+  User can scroll through <p> tag to preview a few lines of each post list.
+  On click of any of the 3 titles page will redirect to with chosen article.
+  
+*/
---
+++ FinalProject/js/homepage.js
@@ -0,0 +1,22 @@
+/*
+HomeP
+Create 3 divs with a class name of "counter". 
+Each div will have an id "cups-counter", "steps-counter","pingpong-counter".
+
+When the page loads up
+  Activate Radial Menu plugin.
+  We are going to update counter every 30 seconds.
+
+In order to update counters:
+  Each div will have a set number to start set in html.
+  Get the current value in each div.
+  Increase the value by a certain number for each div.
+  Cups-count will increase+2
+  Steps-counter will increase+4 
+  Pingpong-counter will increase+1
+
+
+
+
+
+*/
