df85e7371a6bf1aa05d1c4217ac6284b4644a536
Stephanie Santiso <stephlovesdanny@nitrogen-2.local>
2016-06-10 20:17:36 -0400

    Final Additions

--- FinalProject/js/homepage.js
+++ FinalProject/js/homepage.js
@@ -4,7 +4,6 @@ Create 3 divs with a class name of "counter".
 Each div will have an id "cups-counter", "steps-counter","pingpong-counter".
 
 When the page loads up
-  Activate Radial Menu plugin.
   We are going to update counter every 30 seconds.
 
 In order to update counters:
@@ -15,8 +14,43 @@ In order to update counters:
   Steps-counter will increase+4 
   Pingpong-counter will increase+1
 
-
-
-
-
 */
+function updateCounters() {
+  // grab the current text values from divs
+  
+  var pongText = $('#pingpong').text();
+  var stepText = $('#steps').text();
+
+  // convert text values to numbers
+  
+  var pongNumber = parseInt(pongText, 10);
+  var stepNumber = parseInt(stepText, 10);
+
+  // calculate new values
+  
+  var newPongNumber = pongNumber + 1;
+  var newStepNumber = stepNumber + 150;
+
+  // update divs with new values
+  
+  $('#pingpong').text(newPongNumber);
+  $('#steps').text(newStepNumber);
+}
+
+$(function() {
+  setInterval(updateCounters, 10000);
+});
+
+function updateCups(argument) {
+  // grab the current text values from coffee
+  var cupText = $('#cups').text();
+  // convert text values to numbers for coffee
+  var cupNumber = parseInt(cupText, 10);
+  // calculate new values
+  var newCupNumber = cupNumber + 1;
+  // update divs with new values
+  $('#cups').text(newCupNumber);
+}
+$(function() {
+  setInterval(updateCups, 15000);
+});
